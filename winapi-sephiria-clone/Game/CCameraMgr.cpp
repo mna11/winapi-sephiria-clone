2518e71700b31169e568e7b9d0a463e899c62776
MNA11 <thkim306@gmail.com>
2026-09-11 23:49:48 +0900

    feat: CameraMgr 구조 베이스

---
+++ winapi-sephiria-clone/Game/CCameraMgr.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "CCameraMgr.h"
+
+CCameraMgr* CCameraMgr::m_pInstance = nullptr;
+
+CCameraMgr::CCameraMgr()
+	: m_fScrollX(0.f), m_fScrollY(0.f)
+{
+}
+
+CCameraMgr::~CCameraMgr()
+{
+}
+
+void CCameraMgr::Scroll_Lock()
+{
+	if (0.f < m_fScrollX)
+		m_fScrollX = 0.f;
+
+	if (0.f < m_fScrollY)
+		m_fScrollY = 0.f;
+
+	if (WINCX - 1920.f > m_fScrollX)
+		m_fScrollX = WINCX - 1920.f;
+
+	if (WINCY - 1280.f > m_fScrollY)
+		m_fScrollY = WINCY - 1280.f;
+
+}
---
+++ winapi-sephiria-clone/Game/CCameraMgr.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+
+#include "Define.h"
+
+class CCameraMgr
+{
+private:
+	CCameraMgr();
+	~CCameraMgr();
+	CCameraMgr(const CCameraMgr& rhs) = delete;
+	CCameraMgr& operator=(CCameraMgr& rScrollMgr) = delete;
+
+public:
+	static CCameraMgr* GetInstance()
+	{
+		if (!m_pInstance)
+			m_pInstance = new CCameraMgr;
+
+		return m_pInstance;
+	}
+
+	static void	DestroyInstance()
+	{
+		if (m_pInstance)
+		{
+			delete m_pInstance;
+			m_pInstance = nullptr;
+		}
+	}
+
+public:
+	float	GetScrollX() { return m_fScrollX; }
+	float	GetScrollY() { return m_fScrollY; }
+public:
+	void	AddScrollX(float fX) { m_fScrollX += fX; }
+	void	AddScrollY(float fY) { m_fScrollY += fY; }
+
+public:
+	void		Scroll_Lock();
+
+private:
+	static CCameraMgr* m_pInstance;
+
+	float			m_fScrollX;
+	float			m_fScrollY;
+};
