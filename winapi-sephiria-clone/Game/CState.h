0a095ceec7de4af7131176eb7d5e9dabe3172378
MNA11 <thkim306@gmail.com>
2026-09-15 20:21:18 +0900

    feat: 상태 추적 추상 클래스 구현

---
+++ winapi-sephiria-clone/Game/CState.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "Define.h"
+
+template<typename T>
+class CState abstract
+{
+public:
+	CState(T eCurState, T ePreState) : m_eCurState(eCurState), m_ePreState(ePreState) {}
+	virtual ~CState() {}
+public:
+	virtual void ApplyChange() PURE;
+public:
+	void RequestChange(T eState) { m_eCurState = eState; }
+
+protected:
+	T m_eCurState;
+	T m_ePreState;
+};
+
